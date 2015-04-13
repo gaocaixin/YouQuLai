2463434bcf41f6d3d818b6a5d7400cad600ed86a
gaocaixin <qianfeng@gaocaiixn.local>
2015-04-13 19:36:11 +0800

    Initial Commit

---
+++ YouQuLai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YouQuLai
+//
+//  Created by gaocaixin on 15/4/13.
+//  Copyright (c) 2015年 GCX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YouQuLai/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YouQuLai
+//
+//  Created by gaocaixin on 15/4/13.
+//  Copyright (c) 2015年 GCX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
